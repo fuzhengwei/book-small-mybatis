285c49950540f35cb7ebe1f29a6b7e47388d8b9f
付政委 <184172133@qq.com>
2022-07-24 14:01:59 +0800

    小傅哥，feat：第8章：元对象反射工具包

---
+++ mybatis-step-08/src/test/java/cn/bugstack/mybatis/test/ReflectionTest.java
@@ -0,0 +1,106 @@
+package cn.bugstack.mybatis.test;
+
+import cn.bugstack.mybatis.reflection.MetaObject;
+import cn.bugstack.mybatis.reflection.SystemMetaObject;
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 反射类
+ * @date 2022/5/3
+ * @github https://github.com/fuzhengwei/CodeDesignTutorials
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class ReflectionTest {
+
+    private Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    public void test_reflection() {
+        Teacher teacher = new Teacher();
+        List<Teacher.Student> list = new ArrayList<>();
+        list.add(new Teacher.Student());
+        teacher.setName("小傅哥");
+        teacher.setStudents(list);
+
+        MetaObject metaObject = SystemMetaObject.forObject(teacher);
+
+        logger.info("getGetterNames：{}", JSON.toJSONString(metaObject.getGetterNames()));
+        logger.info("getSetterNames：{}", JSON.toJSONString(metaObject.getSetterNames()));
+        logger.info("name的get方法返回值：{}", JSON.toJSONString(metaObject.getGetterType("name")));
+        logger.info("students的set方法参数值：{}", JSON.toJSONString(metaObject.getGetterType("students")));
+        logger.info("name的hasGetter：{}", metaObject.hasGetter("name"));
+        logger.info("student.id（属性为对象）的hasGetter：{}", metaObject.hasGetter("student.id"));
+        logger.info("获取name的属性值：{}", metaObject.getValue("name"));
+        // 重新设置属性值
+        metaObject.setValue("name", "小白");
+        logger.info("设置name的属性值：{}", metaObject.getValue("name"));
+        // 设置属性（集合）的元素值
+        metaObject.setValue("students[0].id", "001");
+        logger.info("获取students集合的第一个元素的属性值：{}", JSON.toJSONString(metaObject.getValue("students[0].id")));
+        logger.info("对象的序列化：{}", JSON.toJSONString(teacher));
+    }
+
+    static class Teacher {
+
+        private String name;
+
+        private double price;
+
+        private List<Student> students;
+
+        private Student student;
+
+        public static class Student {
+
+            private String id;
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public List<Student> getStudents() {
+            return students;
+        }
+
+        public void setStudents(List<Student> students) {
+            this.students = students;
+        }
+
+        public Student getStudent() {
+            return student;
+        }
+
+        public void setStudent(Student student) {
+            this.student = student;
+        }
+    }
+
+}
