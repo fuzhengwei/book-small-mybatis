222cecf471101880fc9f6b395ad05241df2834b9
付政委 <184172133@qq.com>
2022-07-27 20:48:26 +0800

    小傅哥，feat：第21章：整合SpringBoot

--- mybatis-step-21/src/test/java/cn/bugstack/middleware/test/ApiTest.java
+++ mybatis-step-21/src/test/java/cn/bugstack/middleware/test/ApiTest.java
@@ -12,6 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 单元测试
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApiTest {
